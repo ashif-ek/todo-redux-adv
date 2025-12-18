be44e177efd245a788cbbaaf24e117839a5628aa
Ashif EK <ashifek11@gmail.com>
2025-12-18 18:51:01 +0530

    chore: install initial project dependencies.

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,14 +1,16 @@
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', // JSON Server port
+        target: 'https://server-test-1-cogx.onrender.com',
         changeOrigin: true,
+        secure: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
